29f1f854571524a2aa569a898758a216e18fe0ff
MaksSlyzar <maks.slyzar@gmail.com>
2023-10-22 18:46:30 +0300

    add inventory-controller

--- src/GUI/controllers/InventoryController.ts
+++ src/GUI/controllers/InventoryController.ts
@@ -1,6 +1,16 @@
 class InventoryController {
+    slots: number = 20;
+
     constructor () { 
-        
+        const inventoryContainer = document.getElementsByClassName("inventory-conteiner")[0] as HTMLDivElement;
+
+        for (let sotaIndex = 0; sotaIndex < this.slots; sotaIndex++) {
+            const sota = document.createElement("div");
+            sota.id = `sota-${sotaIndex}`;
+            sota.className = "sota";
+
+            inventoryContainer.appendChild(sota);
+        }
     }
 }
 
