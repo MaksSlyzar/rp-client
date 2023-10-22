c10c516cfceb0853187f33f82bb01f387c292490
MaksSlyzar <maks.slyzar@gmail.com>
2023-10-22 19:42:57 +0300

    add count

--- src/GUI/controllers/InventoryController.ts
+++ src/GUI/controllers/InventoryController.ts
@@ -1,3 +1,6 @@
+const sotaPng = require("../../assets/sota-empty.png").default;
+const goldenOre = require("../../assets/golden_ore.png").default;
+
 class InventoryController {
     slots: number = 40;
 
@@ -5,9 +8,23 @@ class InventoryController {
         const inventoryContainer = document.getElementsByClassName("inventory-container")[0] as HTMLDivElement;
 
         for (let sotaIndex = 0; sotaIndex < this.slots; sotaIndex++) {
-            const sota = document.createElement("span");
+            const sota = document.createElement("div");
+            const img = document.createElement("img");
+            const span = document.createElement("span");
+
+            span.innerHTML = "52";
+
+            img.src = goldenOre;
+
             sota.id = `sota-${sotaIndex}`;
             sota.className = "sota";
+            sota.style.backgroundImage = `url(${sotaPng})`;
+
+            span.className = "sota-count";
+
+            img.className = "sota-img";
+            sota.appendChild(img);
+            sota.appendChild(span);
 
             inventoryContainer.appendChild(sota);
         }
